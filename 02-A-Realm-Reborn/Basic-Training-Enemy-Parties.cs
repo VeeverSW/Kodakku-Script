680bf64abf14f7865454fd6919024cfe5e8b7e58
VeeverSW <veever233@outlook.com>
2025-01-04 03:47:53 +0800

    feat: LV.10 完成集团战训练！v0.0.0.1

---
+++ 02-A-Realm-Reborn/Basic-Training-Enemy-Parties.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using Newtonsoft.Json;
+using Dalamud.Utility.Numerics;
+using KodakkuAssist.Script;
+using KodakkuAssist.Module.GameEvent;
+using KodakkuAssist.Module.Draw;
+using ECommons.ExcelServices.TerritoryEnumeration;
+using System.Reflection.Metadata;
+using System.Net;
+using System.Threading.Tasks;
+using Dalamud.Game.ClientState.Objects.Types;
+using System.Runtime.Intrinsics.Arm;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Veever.A_Realm_Reborn.BasicTrainingEnemyParties;
+
+[ScriptType(name: "LV.10 完成集团战训练！", territorys: [214], guid: "6747a004-9234-4b32-9daf-75f6e384061c",
+    version: "0.0.0.1", author: "Veever", note: noteStr)]
+
+public class Basic_Training_Enemy_Parties
+{
+    const string noteStr =
+    """
+    v0.0.0.1:
+    1. 现在支持文字横幅/TTS开关/DR TTS开关（使用DR TTS开关之前请确保你已正确安装`DailyRoutines`插件）（请确保两个TTS开关不要同时打开）
+    鸭门。
+    """;
+    [UserSetting("文字横幅提示开关")]
+    public bool isText { get; set; } = true;
+
+    [UserSetting("TTS开关")]
+    public bool isTTS { get; set; } = true;
+
+    [UserSetting("DR TTS开关")]
+    public bool isDRTTS { get; set; } = false;
+
+    [UserSetting("Debug开关")]
+    public bool isDebug { get; set; } = false;
+
+    public void Init(ScriptAccessory accessory)
+    {
+        accessory.Method.RemoveDraw(".*");
+    }
+
+    [ScriptMethod(name: "飞翼斩", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:1015"])]
+    public void WingCutter(Event @event, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+
+        dp.Name = "Wing Cutter";
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Owner = @event.SourceId();
+        dp.ScaleMode = ScaleMode.ByTime;
+        dp.Scale = new Vector2(7);
+        dp.Radian = float.Pi / 180 * 60;
+        dp.DestoryAt = 2200;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Fan, dp);
+    }
+
+    [ScriptMethod(name: "发霉喷嚏", eventType: EventTypeEnum.StartCasting, eventCondition: ["ActionId:579"])]
+    public void MoldySneeze(Event @event, ScriptAccessory accessory)
+    {
+        var dp = accessory.Data.GetDefaultDrawProperties();
+
+        dp.Name = "Moldy Sneeze";
+        dp.Color = accessory.Data.DefaultDangerColor;
+        dp.Owner = @event.SourceId();
+        dp.ScaleMode = ScaleMode.ByTime;
+        dp.Scale = new Vector2(6);
+        dp.Radian = float.Pi / 180 * 90;
+        dp.DestoryAt = 2500;
+        accessory.Method.SendDraw(DrawModeEnum.Default, DrawTypeEnum.Fan, dp);
+    }
+
+    [ScriptMethod(name: "副本指示", eventType: EventTypeEnum.Director, eventCondition: ["Instance:80032711"])]
+    public void Notify(Event @event, ScriptAccessory accessory)
+    {
+        if (isDebug) accessory.Method.SendChat($"/e {@event.Command()}");
+        if (@event.Command() == 00000000)
+        {
+            if (isText) accessory.Method.TextInfo("在紫圈内等待敌人出现", duration: 4700, true);
+            accessory.TTS("在紫圈内等待敌人出现", isTTS, isDRTTS);
+        }
+    }
+
+}
+
+
+
+
+
+
+
+public static class EventExtensions
+{
+    private static bool ParseHexId(string? idStr, out uint id)
+    {
+        id = 0;
+        if (string.IsNullOrEmpty(idStr)) return false;
+        try
+        {
+            var idStr2 = idStr.Replace("0x", "");
+            id = uint.Parse(idStr2, System.Globalization.NumberStyles.HexNumber);
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public static uint ActionId(this Event @event)
+    {
+        return JsonConvert.DeserializeObject<uint>(@event["ActionId"]);
+    }
+
+    public static uint SourceId(this Event @event)
+    {
+        return ParseHexId(@event["SourceId"], out var id) ? id : 0;
+    }
+
+    public static uint Command(this Event @event)
+    {
+        return ParseHexId(@event["Command"], out var cid) ? cid : 0;
+    }
+
+
+    public static string DurationMilliseconds(this Event @event)
+    {
+        return JsonConvert.DeserializeObject<string>(@event["DurationMilliseconds"]) ?? string.Empty;
+    }
+
+    public static float SourceRotation(this Event @event)
+    {
+        //return ParseHexId(@event["SourceRotation"], out var sourceRotation) ? sourceRotation : 0;
+        return float.TryParse(@event["SourceRotation"], out var rot) ? rot : 0;
+
+    }
+
+    public static byte Index(this Event @event)
+    {
+        return (byte)(ParseHexId(@event["Index"], out var index) ? index : 0);
+    }
+
+    public static uint State(this Event @event)
+    {
+        return ParseHexId(@event["State"], out var state) ? state : 0;
+    }
+
+    public static string SourceName(this Event @event)
+    {
+        return JsonConvert.DeserializeObject<string>(@event["SourceName"]) ?? string.Empty;
+    }
+
+    public static uint TargetId(this Event @event)
+    {
+        return ParseHexId(@event["TargetId"], out var id) ? id : 0;
+    }
+
+    public static Vector3 SourcePosition(this Event @event)
+    {
+        return JsonConvert.DeserializeObject<Vector3>(@event["SourcePosition"]);
+    }
+
+    public static Vector3 TargetPosition(this Event @event)
+    {
+        return JsonConvert.DeserializeObject<Vector3>(@event["TargetPosition"]);
+    }
+
+    public static Vector3 EffectPosition(this Event @event)
+    {
+        return JsonConvert.DeserializeObject<Vector3>(@event["EffectPosition"]);
+    }
+}
+
+
+public static class Extensions
+{
+    public static void TTS(this ScriptAccessory accessory, string text, bool isTTS, bool isDRTTS)
+    {
+        if (isDRTTS)
+        {
+            accessory.Method.SendChat($"/pdr tts {text}");
+        }
+        else if (isTTS)
+        {
+            accessory.Method.TTS(text);
+        }
+    }
+}
