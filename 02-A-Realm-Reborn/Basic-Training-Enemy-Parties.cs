80e7f1914526d36806213b82cc1a400dd9e927a8
VeeverSW <veever233@outlook.com>
2025-09-18 17:22:39 +0800

    feat: api13

--- 02-A-Realm-Reborn/Basic-Training-Enemy-Parties.cs
+++ 02-A-Realm-Reborn/Basic-Training-Enemy-Parties.cs
@@ -6,7 +6,6 @@ using Dalamud.Utility.Numerics;
 using KodakkuAssist.Script;
 using KodakkuAssist.Module.GameEvent;
 using KodakkuAssist.Module.Draw;
-using ECommons.ExcelServices.TerritoryEnumeration;
 using System.Reflection.Metadata;
 using System.Net;
 using System.Threading.Tasks;
@@ -18,13 +17,13 @@ using System.ComponentModel;
 namespace Veever.A_Realm_Reborn.BasicTrainingEnemyParties;
 
 [ScriptType(name: "LV.10 完成集团战训练！", territorys: [214], guid: "6747a004-9234-4b32-9daf-75f6e384061c",
-    version: "0.0.0.2", author: "Veever", note: noteStr)]
+    version: "0.0.0.3", author: "Veever", note: noteStr)]
 
 public class Basic_Training_Enemy_Parties
 {
     const string noteStr =
     """
-    v0.0.0.2:
+    v0.0.0.3:
     1. 现在支持文字横幅/TTS开关/DR TTS开关（使用DR TTS开关之前请确保你已正确安装`DailyRoutines`插件）（请确保两个TTS开关不要同时打开）
     鸭门。
     """;
