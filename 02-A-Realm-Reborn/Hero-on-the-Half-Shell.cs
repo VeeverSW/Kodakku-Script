ab06c085fbc734f7cfb39ad1abed785c9b5c32cf
VeeverSW <veever233@outlook.com>
2025-09-18 17:23:27 +0800

    feat: api13

--- 02-A-Realm-Reborn/Hero-on-the-Half-Shell.cs
+++ 02-A-Realm-Reborn/Hero-on-the-Half-Shell.cs
@@ -6,7 +6,6 @@ using Dalamud.Utility.Numerics;
 using KodakkuAssist.Script;
 using KodakkuAssist.Module.GameEvent;
 using KodakkuAssist.Module.Draw;
-using ECommons.ExcelServices.TerritoryEnumeration;
 using System.Reflection.Metadata;
 using System.Net;
 using System.Threading.Tasks;
@@ -14,18 +13,17 @@ using Dalamud.Game.ClientState.Objects.Types;
 using System.Runtime.Intrinsics.Arm;
 using System.Collections.Generic;
 using System.ComponentModel;
-using ECommons.Reflection;
 
 namespace Veever.A_Realm_Reborn.HeroontheHalfShell;
 
 [ScriptType(name: "LV.15 捕获金币龟！", territorys: [216], guid: "8513e0c9-5a1e-4748-a852-c6150d1c80e4",
-    version: "0.0.0.2", author: "Veever", note: noteStr)]
+    version: "0.0.0.3", author: "Veever", note: noteStr)]
 
 public class Hero_on_the_Half_Shell
 {
     const string noteStr =
     """
-    v0.0.0.2:
+    v0.0.0.3:
     1. 现在支持文字横幅/TTS开关/DR TTS开关（使用DR TTS开关之前请确保你已正确安装`DailyRoutines`插件）（请确保两个TTS开关不要同时打开）
     鸭门。
     """;
