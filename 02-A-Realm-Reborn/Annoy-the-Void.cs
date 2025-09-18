77eed99e07f8b6d3f6a38f67029831005a335ef0
VeeverSW <veever233@outlook.com>
2025-09-18 17:21:44 +0800

    feat: api13

--- 02-A-Realm-Reborn/Annoy-the-Void.cs
+++ 02-A-Realm-Reborn/Annoy-the-Void.cs
@@ -6,7 +6,6 @@ using Dalamud.Utility.Numerics;
 using KodakkuAssist.Script;
 using KodakkuAssist.Module.GameEvent;
 using KodakkuAssist.Module.Draw;
-using ECommons.ExcelServices.TerritoryEnumeration;
 using System.Reflection.Metadata;
 using System.Net;
 using System.Threading.Tasks;
@@ -14,19 +13,18 @@ using Dalamud.Game.ClientState.Objects.Types;
 using System.Runtime.Intrinsics.Arm;
 using System.Collections.Generic;
 using System.ComponentModel;
-using ECommons.Reflection;
 using System.Windows;
 
 namespace Veever.A_Realm_Reborn.AnnoytheVoid;
 
 [ScriptType(name: "LV.30 讨伐坑道中出现的妖异！", territorys: [222], guid: "9106a502-2bcb-41ac-8598-6c76bd1061ae",
-    version: "0.0.0.2", author: "Veever", note: noteStr)]
+    version: "0.0.0.3", author: "Veever", note: noteStr)]
 
 public class Annoy_the_Void
 {
     const string noteStr =
     """
-    v0.0.0.2:
+    v0.0.0.3:
     1. 现在支持文字横幅/TTS开关/DR TTS开关（使用DR TTS开关之前请确保你已正确安装`DailyRoutines`插件）（请确保两个TTS开关不要同时打开）
     2. 标点开关以及本地开关都在用户设置里面，可自行选择关闭或者开启（默认本地开启）
     鸭门。
