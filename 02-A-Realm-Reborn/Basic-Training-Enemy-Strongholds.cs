fdb6a139daaccdb83bf07dcb0caaac6bccaa5123
VeeverSW <veever233@outlook.com>
2025-09-18 17:22:53 +0800

    feat: api13

--- 02-A-Realm-Reborn/Basic-Training-Enemy-Strongholds.cs
+++ 02-A-Realm-Reborn/Basic-Training-Enemy-Strongholds.cs
@@ -6,7 +6,6 @@ using Dalamud.Utility.Numerics;
 using KodakkuAssist.Script;
 using KodakkuAssist.Module.GameEvent;
 using KodakkuAssist.Module.Draw;
-using ECommons.ExcelServices.TerritoryEnumeration;
 using System.Reflection.Metadata;
 using System.Net;
 using System.Threading.Tasks;
@@ -14,18 +13,17 @@ using Dalamud.Game.ClientState.Objects.Types;
 using System.Runtime.Intrinsics.Arm;
 using System.Collections.Generic;
 using System.ComponentModel;
-using ECommons.Reflection;
 
 namespace Veever.A_Realm_Reborn.BasicTrainingEnemyStrongholds;
 
 [ScriptType(name: "LV.15 突破所有关门，讨伐最深处的敌人！", territorys: [215], guid: "72889f91-2654-400c-9112-69d4a063557c",
-    version: "0.0.0.2", author: "Veever", note: noteStr)]
+    version: "0.0.0.3", author: "Veever", note: noteStr)]
 
 public class Basic_Training_Enemy_Strongholds
 {
     const string noteStr =
     """
-    v0.0.0.2:
+    v0.0.0.3:
     1. 现在支持文字横幅/TTS开关/DR TTS开关（使用DR TTS开关之前请确保你已正确安装`DailyRoutines`插件）（请确保两个TTS开关不要同时打开）
     鸭门。
     """;
